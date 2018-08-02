d84fcb0d5cad867aa21709db36dcb8794c11eac4
WyattClark <40504621+WyattClark@users.noreply.github.com>
2018-08-01 23:33:36 -0700

    PygameFolderAdded
    I installed pygame from pip but this folder still inclusdes the examples that may be needed.

---
+++ Graphics/Window.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+import os
+
+#Seting up my window
+Screen_Size = (1000, 1000)
+screen = pygame.display.set_mode(Screen_Size)
+pygame.display.set_caption("My First Window")# Define some colors
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done = True # Flag that we are done so we exit this loop
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
